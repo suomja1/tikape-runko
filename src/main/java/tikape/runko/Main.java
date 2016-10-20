9c037c504186cd0efa8f32a0c283f4a3189dfd18
Huotari Maija P <maijhuot@da5-cs-b221-15.helsinki.fi>
2016-10-20 12:10:52 +0300

    Viesti

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -1,6 +1,8 @@
 package tikape.runko;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
@@ -10,21 +12,28 @@ import tikape.runko.domain.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        
+
         if (System.getenv("PORT") != null) {
             port(Integer.valueOf(System.getenv("PORT")));
         }
-        
-        Database database = new Database("jdbc:sqlite:foorumi.db");
-        
+
+        // käytetään oletuksena paikallista sqlite-tietokantaa
+        String jdbcOsoite = "jdbc:sqlite:foorumi.db";
+        // jos heroku antaa käyttöömme tietokantaosoitteen, otetaan se käyttöön
+        if (System.getenv("DATABASE_URL") != null) {
+            jdbcOsoite = System.getenv("DATABASE_URL");
+        }
+
+        Database database = new Database(jdbcOsoite);
+
         KeskustelualueDao keskustelualuedao = new KeskustelualueDao(database);
         KeskustelunavausDao keskustelunavausdao = new KeskustelunavausDao(database);
         VastausDao vastausdao = new VastausDao(database);
-        
+
         //Pyyntö juuriosoitteeseen listaa kaikki keskustelualueet
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            
+
             map.put("alueet", keskustelualuedao.findAll());
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
@@ -32,14 +41,14 @@ public class Main {
         //Keskustelualueen lisääminen
         post("/", (req, res) -> {
             keskustelualuedao.create(new Keskustelualue(
-                    req.queryParams("aihealue"), 
+                    req.queryParams("aihealue"),
                     req.queryParams("perustaja")
             ));
-            
+
             res.redirect("/");
             return "";
         });
-        
+
         //Keskustelualuekohtaiset sivut
         get("/:id", (req, res) -> {
             HashMap map = new HashMap<>();
@@ -49,21 +58,21 @@ public class Main {
             map.put("avaukset", keskustelunavausdao.findAll(id));
             return new ModelAndView(map, "alue");
         }, new ThymeleafTemplateEngine());
-        
+
         //Keskustelualueen lisääminen
         post("/:id", (req, res) -> {
             Integer id = Integer.parseInt(req.params(":id"));
             keskustelunavausdao.create(new Keskustelunavaus(
-                    keskustelualuedao.findOne(id), 
-                    req.queryParams("otsikko"), 
-                    req.queryParams("avaus"), 
+                    keskustelualuedao.findOne(id),
+                    req.queryParams("otsikko"),
+                    req.queryParams("avaus"),
                     req.queryParams("aloittaja")
             ));
-            
+
             res.redirect("/" + id);
             return "";
         });
-        
+
         //Keskustelunavauskohtaiset sivut
         get("/:id/:idd", (req, res) -> {
             HashMap map = new HashMap<>();
@@ -75,19 +84,21 @@ public class Main {
             map.put("viestit", vastausdao.findAll(idd));
             return new ModelAndView(map, "avaus");
         }, new ThymeleafTemplateEngine());
-        
+
         //Vastauksen lisääminen
         post("/:id/:idd", (req, res) -> {
             Integer id = Integer.parseInt(req.params(":id"));
             Integer idd = Integer.parseInt(req.params(":idd"));
             vastausdao.create(new Vastaus(
-                    keskustelunavausdao.findOne(idd), 
-                    req.queryParams("teksti"), 
+                    keskustelunavausdao.findOne(idd),
+                    req.queryParams("teksti"),
                     req.queryParams("kirjoittaja")
             ));
-            
+
             res.redirect("/" + id + "/" + idd);
             return "";
         });
     }
+
+    
 }
--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -8,6 +8,32 @@ public class Database<T> {
 
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.address = databaseAddress;
+        
+        init();
+    }
+    
+     private void init() {
+        List<String> lauseet = null;
+        if (this.address.contains("postgres")) {
+            lauseet = postgreLauseet();
+        } else {
+            lauseet = sqliteLauseet();
+        }
+
+        // "try with resources" sulkee resurssin automaattisesti lopuksi
+        try (Connection conn = getConnection()) {
+            Statement st = conn.createStatement();
+
+            // suoritetaan komennot
+            for (String lause : lauseet) {
+                System.out.println("Running command >> " + lause);
+                st.executeUpdate(lause);
+            }
+
+        } catch (Throwable t) {
+            // jos tietokantataulu on jo olemassa, ei komentoja suoriteta
+            System.out.println("Error >> " + t.getMessage());
+        }
     }
     
     public Connection getConnection() throws SQLException {
@@ -61,5 +87,61 @@ public class Database<T> {
         conn.close();
 
         return rows;
+    }
+    
+    private List<String> postgreLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+
+        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
+        lista.add("DROP TABLE Keskustelualue;");
+        // heroku käyttää SERIAL-avainsanaa uuden tunnuksen automaattiseen luomiseen
+        lista.add("CREATE TABLE Keskustelualue (id Integer SERIAL PRIMARY KEY, aihealue varchar(64) NOT NULL, "
+                + "kuvaus varchar(128), perustettu timestamp default current_timestamp NOT NULL, "
+                + "perustaja varchar(16) NOT NULL);");
+        lista.add("INSERT INTO Keskustelualue (aihealue, kuvaus, perustettu, perustaja) VALUES ('postgresql-keskustelualue');");
+        
+        lista.add("DROP TABLE Keskustelunavaus;");
+        
+        lista.add("CREATE TABLE Keskustelunavaus (id Integer SERIAL PRIMARY KEY, alue Integer NOT NULL, "
+                + "otsikko varchar(64) NOT NULL, avaus varchar NOT NULL, "
+                + "aloitettu timestamp default current_timestamp NOT NULL, "
+                + "aloittaja varchar(16) NOT NULL, FOREIGN KEY(alue) REFERENCES Keskustelualue)");
+        
+        lista.add("INSERT INTO Keskustelunavaus (alue, otsikko, avaus, aloitettu, aloittaja) VALUES ('postgresql-keskustelunavaus');");
+        
+        lista.add("DROP TABLE Vastaus;");
+        
+        lista.add("CREATE TABLE Vastaus (id Integer SERIAL PRIMARY KEY, avaus Integer NOT NULL, "
+                + "teksti varchar NOT NULL, ajankohta timestamp default current_timestamp NOT NULL, "
+                + "kirjoittaja varchar(16) NOT NULL, FOREIGN KEY(avaus) REFERENCES Keskustelunavaus)");
+        
+        lista.add("INSERT INTO Vastaus (avaus, teksti, ajankohta, kirjoittaja) VALUES ('postgresql-keskustelunavaus');");
+
+        return lista;
+    }
+
+    private List<String> sqliteLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+
+        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
+        lista.add("CREATE TABLE Keskustelualue (id Integer PRIMARY KEY, "
+                + "aihealue varchar(64) NOT NULL, kuvaus varchar(128), "
+                + "perustettu timestamp default current_timestamp NOT NULL, "
+                + "perustaja varchar(16) NOT NULL)");
+        lista.add("INSERT INTO Keskustelualue (aihealue, kuvaus, perustettu, perustaja) "
+                + "VALUES ('sqlite-keskustelualue');");
+        
+        lista.add("CREATE TABLE Keskustelunavaus (id Integer PRIMARY KEY, alue Integer NOT NULL, "
+                + "otsikko varchar(64) NOT NULL, avaus varchar NOT NULL, "
+                + "aloitettu timestamp default current_timestamp NOT NULL, "
+                + "aloittaja varchar(16) NOT NULL, FOREIGN KEY(alue) REFERENCES Keskustelualue);");
+        lista.add("INSERT INTO Keskustelunavaus (alue, otsikko, avaus, aloitettu, aloittaja) VALUES ('sqlite-keskustelunavaus');");
+        
+        lista.add("CREATE TABLE Vastaus (id Integer PRIMARY KEY, avaus Integer NOT NULL, "
+                + "teksti varchar NOT NULL, ajankohta timestamp default current_timestamp NOT NULL, "
+                + "kirjoittaja varchar(16) NOT NULL, FOREIGN KEY(avaus) REFERENCES Keskustelunavaus);");
+        lista.add("INSERT INTO Vastaus (avaus, teksti, ajankohta, kirjoittaja) VALUES ('sqlite-keskustelunavaus');");
+        
+        return lista;
     }
 }
